89016527a1d5d214fb92c0b676ca11f685b6df24
Logan McNaughton <logan@bacoosta.com>
2017-02-19 22:16:05 -0700

    Fix crash when opening 2nd rom

--- workerthread.h
+++ workerthread.h
@@ -2,14 +2,17 @@
 #define WORKERTHREAD_H
 
 #include <QThread>
+#include <QApplication>
 #include <QString>
 #include "common.h"
+#include "vidext.h"
 
 class WorkerThread : public QThread
 {
     Q_OBJECT
     void run() Q_DECL_OVERRIDE {
         openROM(m_fileName);
+        my_window->context()->moveToThread(QApplication::instance()->thread());
     }
 public:
     void setFileName(QString filename) {
