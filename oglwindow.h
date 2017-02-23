fcd9b796512efd3b9b1153e5716d76a32c3219c1
Logan McNaughton <logan@bacoosta.com>
2017-02-23 11:49:24 -0800

    Fix garbage screen output

--- oglwindow.h
+++ oglwindow.h
@@ -3,13 +3,20 @@
 
 #include <QCloseEvent>
 #include <QOpenGLWindow>
+#include <QOpenGLFunctions>
 #include "common.h"
 #include "core_interface.h"
 
 class OGLWindow : public QOpenGLWindow
 {
 protected:
-    void exposeEvent(QExposeEvent *) Q_DECL_OVERRIDE {}
+    void exposeEvent(QExposeEvent *) Q_DECL_OVERRIDE {
+        if (!exposed) {
+            context()->functions()->glViewport(0, 0, width(), height());
+            context()->swapBuffers(this);
+            exposed = 1;
+        }
+    }
 
     void keyPressEvent(QKeyEvent *event)
     {
@@ -24,6 +31,8 @@ protected:
         if (keyValue != -1 && QtAttachCoreLib())
             (*CoreDoCommand)(M64CMD_SEND_SDL_KEYUP, keyValue, NULL);
     }
+private:
+    int exposed = 0;
 };
 
 #endif // OGLWINDOW_H
