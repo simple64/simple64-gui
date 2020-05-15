0a246fbb1d805a10c04d8f1f3630917e714e8ec6
Logan <loganmc10@users.noreply.github.com>
2020-05-15 16:00:19 -0600

    forgot the rest of them

--- netplay/joinroom.cpp
+++ netplay/joinroom.cpp
@@ -203,10 +203,13 @@ void JoinRoom::processBinaryMessage(QByteArray message)
         newItem->setFlags(newItem->flags() & ~Qt::ItemIsEditable);
         listWidget->setItem(row, 0, newItem);
         newItem = new QTableWidgetItem(json.value("game_name").toString());
+        newItem->setFlags(newItem->flags() & ~Qt::ItemIsEditable);
         listWidget->setItem(row, 1, newItem);
         newItem = new QTableWidgetItem(json.value("MD5").toString());
+        newItem->setFlags(newItem->flags() & ~Qt::ItemIsEditable);
         listWidget->setItem(row, 2, newItem);
         newItem = new QTableWidgetItem(json.value("protected").toString());
+        newItem->setFlags(newItem->flags() & ~Qt::ItemIsEditable);
         listWidget->setItem(row, 3, newItem);
 
         ++row;
