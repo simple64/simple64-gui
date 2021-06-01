e6cec8ed86cafb5ebc45ec0b102a77f4334be2d3
Logan <loganmc10@users.noreply.github.com>
2021-06-01 08:24:49 -0600

    Update osal_dynamiclib_win32.c

--- osal/osal_dynamiclib_win32.c
+++ osal/osal_dynamiclib_win32.c
@@ -32,8 +32,8 @@ m64p_error osal_dynlib_open(m64p_dynlib_handle *pLibHandle, const char *pccLibra
     if (pLibHandle == NULL || pccLibraryPath == NULL)
         return M64ERR_INPUT_ASSERT;
 
-    wchar_t wstr[256];
-    MultiByteToWideChar(CP_UTF8, 0, pccLibraryPath, -1, wstr, 256);
+    wchar_t wstr[1024];
+    MultiByteToWideChar(CP_UTF8, 0, pccLibraryPath, -1, wstr, 1024);
     *pLibHandle = LoadLibrary(wstr);
 
     if (*pLibHandle == NULL)
