c037c41d2e4c5927283560ce43957054676076c4
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-07-31 14:30:22 -0600

    remove globals

--- hotkeydialog.cpp
+++ hotkeydialog.cpp
@@ -6,18 +6,12 @@
 #include <QLabel>
 #include <QScrollArea>
 #include <QSettings>
-#include <QTabWidget>
-#include <QGridLayout>
-#include <QValidator>
 #include <QMessageBox>
-#include "settingclasses.h"
-
-m64p_handle coreEventsConfigHandle;
-QGridLayout *coreEventsLayout;
-int coreEventsLayoutRow;
 
 static void paramListCallback(void *context, const char *ParamName, m64p_type ParamType)
 {
+    HotkeyDialog* dialog = (HotkeyDialog*)context;
+
     if (strcmp((char*)ParamName, "Version") == 0) return;
     else if (strncmp((char*)ParamName, "Joy Mapping", 11) == 0) return;
 
@@ -25,34 +19,35 @@ static void paramListCallback(void *context, const char *ParamName, m64p_type Pa
     bool l_ParamBool;
     float l_ParamFloat;
     QString l_ParamString;
-    QString helper = (*ConfigGetParameterHelp)(coreEventsConfigHandle, ParamName);
-    QLabel *desc = new QLabel(ParamName, coreEventsLayout->parentWidget());
+    QString helper = (*ConfigGetParameterHelp)(dialog->getHandle(), ParamName);
+    QLabel *desc = new QLabel(ParamName, dialog->getLayout()->parentWidget());
     if (!helper.isEmpty()) {
        helper.prepend("<span style=\"color:black;\">");
        helper.append("</span>");
        desc->setToolTip(helper);
     }
     desc->setStyleSheet("border: 1px solid; padding: 10px");
-    coreEventsLayout->addWidget(desc, coreEventsLayoutRow, 0);
-    CustomButton *my_Widget = new CustomButton(coreEventsLayout->parentWidget());
-    my_Widget->setConfigHandle(coreEventsConfigHandle);
+    dialog->getLayout()->addWidget(desc, *dialog->getLayoutRow(), 0);
+    CustomButton *my_Widget = new CustomButton(dialog->getLayout()->parentWidget());
+    dialog->getButtonList()->append(my_Widget);
+    my_Widget->setConfigHandle(dialog->getHandle());
     my_Widget->setParamType(ParamType);
     my_Widget->setParamName(ParamName);
-    l_ParamInt = (*ConfigGetParamInt)(coreEventsConfigHandle, ParamName);
+    l_ParamInt = (*ConfigGetParamInt)(dialog->getHandle(), ParamName);
     my_Widget->setText(QKeySequence(SDL22QT(sdl_keysym2scancode(l_ParamInt))).toString());
 
-    coreEventsLayout->addWidget((QWidget*)my_Widget, coreEventsLayoutRow, 1);
+    dialog->getLayout()->addWidget((QWidget*)my_Widget, *dialog->getLayoutRow(), 1);
 
-    ClearButton* clear_Widget = new ClearButton(coreEventsLayout->parentWidget());
-    clear_Widget->setConfigHandle(coreEventsConfigHandle);
+    ClearButton* clear_Widget = new ClearButton(dialog->getLayout()->parentWidget());
+    clear_Widget->setConfigHandle(dialog->getHandle());
     clear_Widget->setParamType(ParamType);
     clear_Widget->setParamName(ParamName);
     clear_Widget->setMainButton(my_Widget);
-    l_ParamInt = (*ConfigGetParamInt)(coreEventsConfigHandle, ParamName);
+    l_ParamInt = (*ConfigGetParamInt)(dialog->getHandle(), ParamName);
     clear_Widget->setText("Clear");
 
-    coreEventsLayout->addWidget((QWidget*)clear_Widget, coreEventsLayoutRow, 2);
-    ++coreEventsLayoutRow;
+    dialog->getLayout()->addWidget((QWidget*)clear_Widget, *dialog->getLayoutRow(), 2);
+    ++*dialog->getLayoutRow();
 }
 
 void HotkeyDialog::handleResetButton()
@@ -77,15 +72,15 @@ HotkeyDialog::HotkeyDialog(QWidget *parent)
 {
     m64p_error res;
 
-    coreEventsLayoutRow = 0;
+    m_layoutRow = 0;
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
 
     QWidget *coreEventsSettings = new QWidget(this);
-    coreEventsLayout = new QGridLayout(coreEventsSettings);
-    coreEventsSettings->setLayout(coreEventsLayout);
-    res = (*ConfigOpenSection)("CoreEvents", &coreEventsConfigHandle);
+    m_layout = new QGridLayout(coreEventsSettings);
+    coreEventsSettings->setLayout(m_layout);
+    res = (*ConfigOpenSection)("CoreEvents", &m_configHandle);
     if (res == M64ERR_SUCCESS)
-        (*ConfigListParameters)(coreEventsConfigHandle, (char*)"CoreEvents", paramListCallback);
+        (*ConfigListParameters)(m_configHandle, this, paramListCallback);
     QScrollArea *coreEventsScroll = new QScrollArea(this);
     coreEventsScroll->setWidget(coreEventsSettings);
     coreEventsScroll->setMinimumWidth(coreEventsSettings->sizeHint().width() + 20);
@@ -100,4 +95,24 @@ HotkeyDialog::HotkeyDialog(QWidget *parent)
     connect(resetButton, SIGNAL (released()),this, SLOT (handleResetButton()));
     mainLayout->addWidget(resetButton);
     setLayout(mainLayout);
+}
+
+QList<CustomButton*>* HotkeyDialog::getButtonList()
+{
+    return &m_coreEventsButtonList;
+}
+
+m64p_handle HotkeyDialog::getHandle()
+{
+    return m_configHandle;
+}
+
+QGridLayout* HotkeyDialog::getLayout()
+{
+    return m_layout;
+}
+
+int* HotkeyDialog::getLayoutRow()
+{
+    return &m_layoutRow;
 }
--- hotkeydialog.h
+++ hotkeydialog.h
@@ -2,14 +2,25 @@
 #define HOTKEYDIALOG_H
 
 #include <QDialog>
+#include <QGridLayout>
+#include "settingclasses.h"
 
 class HotkeyDialog : public QDialog
 {
     Q_OBJECT
 public:
     HotkeyDialog(QWidget *parent = nullptr);
+    QList<CustomButton*>* getButtonList();
+    m64p_handle getHandle();
+    QGridLayout* getLayout();
+    int* getLayoutRow();
 private slots:
     void handleResetButton();
+private:
+    QList<CustomButton*> m_coreEventsButtonList;
+    m64p_handle m_configHandle;
+    QGridLayout *m_layout;
+    int m_layoutRow;
 };
 
 #endif // HOTKEYDIALOG_H
